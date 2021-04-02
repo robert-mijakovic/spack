70defe935dcdfe57414d40d310960b7e72c6e02a
Robert Mijakovic <robert.mijakovic@lrz.de>
2021-04-02 15:47:01 +0200

    visit: new version

--- var/spack/repos/builtin/packages/visit/package.py
+++ var/spack/repos/builtin/packages/visit/package.py
@@ -61,6 +61,7 @@ class Visit(CMakePackage):
     maintainers = ['cyrush']
 
     version('develop', branch='develop')
+    version('3.1.4', sha256='be20d9acf56f0599e3c511709f48d8d3b232a57425f69d2bd1e2df1eccb84c93')
     version('3.1.1', sha256='0b60ac52fd00aff3cf212a310e36e32e13ae3ca0ddd1ea3f54f75e4d9b6c6cf0')
     version('3.0.1', sha256='a506d4d83b8973829e68787d8d721199523ce7ec73e7594e93333c214c2c12bd')
     version('2.13.3', sha256='cf0b3d2e39e1cd102dd886d3ef6da892733445e362fc28f24d9682012cccf2e5')
